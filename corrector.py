42a6c172d82d50c14fd1d0aa62f52c1bfa4fadfc
Mai Truong <maitruong2801@gmail.com>
2016-11-23 13:58:56 -0500

    Establised function structure

---
+++ corrector.py
@@ -0,0 +1,37 @@
+import re
+
+def P(word):
+	'''
+	Probability that word appears in English text
+	'''
+def correction(word):
+	'''
+	Most probable spelling correction for word.
+	This will be the main function
+	'''
+	pass
+
+def candidates(word):
+	'''
+	Candidates of spelling corrections to consider. We will check these in the
+	dictionary
+	'''
+	pass
+
+def known(word):
+	'''
+	The subset of words that appear in the dictionary
+	'''
+	pass
+
+def edits1(word):
+	'''
+	Create the corrections based on edits that are one step away from `word`
+	'''
+	pass
+
+def edits2(word):
+	'''
+	Create edits that are 2 steps away from word
+	'''
+	pass
