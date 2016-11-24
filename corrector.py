3ffcd5e4138a3841ee2b4bb0f5f79b824e5a5e3a
Mai Truong <maitruong2801@gmail.com>
2016-11-24 10:25:25 -0800

    fixed the file path issues and created dict of words frequencies

--- corrector.py
+++ corrector.py
@@ -1,9 +1,26 @@
 import re
+import sys, os
+from collections import Counter
+#Counter is dict subclass for counting
+# Internally represented as a dict with keys as words and values as word occurrences
+
+fileName = 'dictionary.txt'
+filePath = os.path.join(os.path.dirname(__file__), fileName)
+print filePath
+
+def words(text):
+	return re.findall(r'\w+', text.lower())
+
+WORDS = Counter(words(open(filePath, 'r').read()))
 
 def P(word):
 	'''
 	Probability that word appears in English text
 	'''
+	N = sum(WORDS.values())
+	return WORDS[word] / N
+	pass
+
 def correction(word):
 	'''
 	Most probable spelling correction for word.
@@ -18,26 +35,35 @@ def candidates(word):
 	'''
 	pass
 
-def known(word):
+def known(words):
 	'''
 	The subset of words that appear in the dictionary
+	Receives the lists of modified words from edits1 and check the words against the 
+	dictionary. Returns the words that exist
 	'''
-	pass
+	return set([w for w in words if w in WORDS])
 
 def edits1(word):
 	'''
 	Create the corrections based on edits that are one step away from `word`
 	'''
 	letters = 'abcdefghijklmnopqrstuvwxyz'
-	splits = [(word[:i], word[i:]) for i in range(length(word) + 1)]
+	splits = [(word[:i], word[i:]) for i in range(len(word) + 1)]
 	deletes = [L + R[1:] for L, R in splits if R]
-	transposes = [L + R[1] + R[0] + R[2:] for L, R in splits if len(R) > 2]
+	transposes = [L + R[1] + R[0] + R[2:] for L, R in splits if len(R) > 1]
 	replaces = [L + w + R[1:] for L, R in splits if R for w in letters]
-	inserts = [ L + w + R for L, R in splits for w in letter]
-
+	inserts = [ L + w + R for L, R in splits for w in letters]
+	return set(deletes + transposes + replaces + inserts) # Build set from list of lists
 
 def edits2(word):
 	'''
 	Create edits that are 2 steps away from word
 	'''
 	pass
+
+print len(edits1('somthing'))
+#known(edits1('somthing'))
+
+#f.write('To test out corrector.py')
+#print sys.argv
+
