51748d95bc31e70463968281a76fab7355993908
Mai Truong <maitruong2801@gmail.com>
2016-11-24 12:18:30 -0800

    deleted unittest.py because its name clashed with standard library name

--- unittest.py
+++ unittest.py
@@ -4,18 +4,23 @@ Some unit tests for the corrector
 import unittest
 
 class CorrectTest(unittest.TestCase):
-	"""docstring for CorrectTest"""
-	def testEquals(self):
-		self.assertEqual(correction('speling'), 'spelling')
-		self.assertEqual(correction('korrectud'), 'corrected')
-		self.assertEqual(correction('bycycle'), 'bicycle')
-		self.assertEqual(correction('inconvient'), 'inconvenient')
-		self.assertEqual(correction('arrainged'), 'arranged')
-		self.assertEqual(correction('peotry'), 'poetry')
-		self.assertEqual(correction('peotryy'), 'poetry')
-		self.assertEqual(correction('word'), 'word')
-		self.assertEqual(correction('quintessential'), 'quintessential')
-		self.assertEqual(correction('moring'), 'morning')
+	"""Test cases for Corrector"""
+	def setUp(self):
+		self.corrector = Corrector()
+		
+	def test_equals(self):
+		self.assertEqual(self.corrector.correction('speling'), 'spelling')					# insert
+		self.assertEqual(self.corrector.correction('korrectud'), 'corrected')				# replace 2
+		self.assertEqual(self.corrector.correction('bycycle'), 'bicycle')					# replace
+		self.assertEqual(self.corrector.correction('inconvient'), 'inconvenient')			# insert 2
+		self.assertEqual(self.corrector.correction('arrainged'), 'arranged')				# delete
+		self.assertEqual(self.corrector.correction('peotry'), 'poetry')					# transpose
+		self.assertEqual(self.corrector.correction('peotryy'), 'poetry')					# transpose + delete
+		self.assertEqual(self.corrector.correction('word'), 'word')						# known
+		self.assertEqual(self.corrector.correction('quintessential'), 'quintessential')	# unknown
+		self.assertEqual(self.corrector.correction('moring'), 'morning')					# insert
+		self.assertEqual(len(WORDS.values()), 32198)
+		self.assertEqual(WORDS['the'], 79809)
 
 if __name__ == '__main__':
 	unittest.main()
